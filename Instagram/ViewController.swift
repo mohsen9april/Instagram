f45af9ed820c4d415b0fc32e6f8ada3ec79ccad1
Mohsen Abdollahi <mohsenabdollahi@mohsens-macbook-pro.local>
2019-06-15 13:53:41 +0430

    Instagram Lesson 1

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -9,12 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Set Closure plusPhotoButton
+    let plusPhotoButton : UIButton = {
+        let button = UIButton(type : .system)
+        button.setImage(#imageLiteral(resourceName: "plus-photo-button"), for: .normal)
+        return button
+    }()
+    
+    //Set Closure 3 TextFileds
+    let emailTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let usernameTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let passwordTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    //Set Closure Signup Button
+    let signUPButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a Button with AutoLayout Option
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.translatesAutoresizingMaskIntoConstraints = false
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
+        //Create a StackView
+        setupInputFields()
+
     }
-
-
+    
+    
+    fileprivate func setupInputFields() {
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField,usernameTextField,passwordTextField,signUPButton])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing  = 12
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20).isActive = true
+        stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
 }
-
